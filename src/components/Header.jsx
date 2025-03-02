e449501bb857eec59ce41ae29853fa96c8f640e9
aayushidumka <aayushi.dumka@gmail.com>
2025-03-01 22:21:28 -0600

    Reverted commit 8414c6ff04221459c99768adcd889c08a19bda4b

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { Link } from "react-router-dom";
 import "../App.css"; // Correct relative path
 import React from 'react'
 
@@ -6,22 +7,7 @@ import React from 'react'
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 
-// auth
-import { useAuthContext } from "@asgardeo/auth-react";
-import { Link, useNavigate } from "react-router-dom"; // will make sure that the page reloads
-
 const Header = () => {
-
-  // auth
-  const { state, signOut } = useAuthContext();
-  const navigate = useNavigate(); // Hook to navigate programmatically
-
-  // Handle logout with redirect
-  const handleLogout = async () => {
-    await signOut();
-    navigate("/"); // Redirect to home after logout
-    window.location.reload(); // Force refresh to re-evaluate auth state
-  };
 
   // handle input from context
   const { inputValue } = useContext(AppContext);
@@ -30,13 +16,6 @@ const Header = () => {
   return (
     <AppBar position="static" sx={{ backgroundColor: "maroon" }}>
       <Toolbar>
-        <img
-          src="https://www.svgrepo.com/show/9074/clown-fish.svg"
-          alt="Logo"
-          style={{ height: "50px", marginRight: "20px" }} // adjust the size and spacing
-        />
-        
-        
         {/* Title */}
 
         <Typography variant="h5"  sx={{ flexGrow: 1 }}>
@@ -52,17 +31,27 @@ const Header = () => {
             Employee Management
           </Button>
         </Box>
-
-        {/* Show "Log Out" button if user is logged in */}
-        {state?.isAuthenticated && (
-          <Button color="inherit" onClick={handleLogout} sx={{ marginLeft: "auto" }}>
-            Sign Out
-          </Button>
-        )}
-
       </Toolbar>
     </AppBar>
   )
 }
 
 export default Header
+
+// function Header() {
+//   const titleName = "Aayushi";
+
+//   return (
+//     <header>
+//       <h1 id="header-cust">Welcome {titleName} to CodeCraft Intranet</h1>
+//       <nav>
+//         <ul>
+//           <li><Link to="/">Home</Link></li>
+//           <li><Link to="/employee-mgmt">Employee Management</Link></li>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// }
+
+// export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,3 @@
-// import asgardeo
-import { AuthProvider } from "@asgardeo/auth-react";
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
@@ -10,20 +7,8 @@ import { AppProvider } from "./context/AppContext";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* wrap app in authprovider */}
-    <AuthProvider
-        config={ {
-            signInRedirectURL: "http://localhost:5173/",
-            signOutRedirectURL: "http://localhost:5173/",
-            clientID: "9gQd8aPfanyHWS6gdDDZaNfowEYa",
-            baseUrl: "https://api.asgardeo.io/t/aayushidumka",
-            scope: [ "openid","profile" ]
-        } }
-    >
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </AuthProvider>
-
+    <AppProvider>
+       <App />
+    </AppProvider>
   </StrictMode>,
 )
